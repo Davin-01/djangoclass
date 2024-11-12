1cb825027b3edb4a98c79e99f8ac86620be4d84f
Davin-01 <moengadavid90@gmail.com>
2024-11-12 13:28:30 +0300

    updatedcode

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
+
+
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('faq/', views.faq, name='faq'),  # Corrected the view name and path name
-    path('products/', views.products, name='products'),
-   # path('product-detail/', views.productdetail, name='product-detail'),
-    path('sign-in/', views.signin, name='sign-in'),
-    path('sign-up/', views.signup, name='sign-up'),
+urlpatterns=[
+    path('index.html',views.home,name='home'),
+    path('about.html',views.about,name='about'),
+    path('products.html',views.products,name='products'),
+    path('store.html',views.store,name='store'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request,'index.html')
 def about(request):
-    return render(request, 'about.html')
-def contact(request):
-    return render(request, 'contact.html')
-def faq(request):
-    return render(request, 'faq.html')
+    return render(request,'about.html')
 def products(request):
-    return render(request, 'products.html')
-def signup(request):
-    return render(request,'sign-up.html')
-def signin(request):
-    return render(request,'sign-in.html')
-#def productdetail(request):
-    #return render(request,'product-detail.html')
+    return render(request,'products.html')
+def store(request):
+    return render(request,'store.html')
